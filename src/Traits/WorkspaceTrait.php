91c9822831dc5bb86d28c3ce88a5afce53cf1f40
Olivier Chatelain <olivier.chatelain@gmail.com>
2019-06-11 08:23:49 +0200

    Add: Fct. to list all user workspaces.

--- src/Traits/WorkspaceTrait.php
+++ src/Traits/WorkspaceTrait.php
@@ -10,6 +10,17 @@ trait WorkspaceTrait
      *
      * @return stdClass
      */
+    public function workspaces()
+    {
+        return $this->sendGetMessage('https://www.toggl.com/api/v8/workspaces');
+
+    }
+
+    /**
+     * Get workspace projects
+     *
+     * @return stdClass
+     */
     public function workspaceProjects()
     {
         return $this->sendGetMessage('https://www.toggl.com/api/v8/workspaces/' . $this->workspaceId . '/projects');
