50149860f25c0e4e05d507dd9046e643f3aa2ea6
Matt Watson <matt@recycledbeans.com>
2021-04-14 06:44:50 -0400

    Add missing workspace API GET methods

--- src/Traits/WorkspaceTrait.php
+++ src/Traits/WorkspaceTrait.php
@@ -13,7 +13,26 @@ trait WorkspaceTrait
     public function workspaces()
     {
         return $this->sendGetMessage($this->baseUrl .'/v8/workspaces');
-
+    }
+    
+    /**
+     * Get workspace clients
+     *
+     * @return stdClass
+     */
+    public function workspaceClients()
+    {
+        return $this->sendGetMessage($this->baseUrl .'/v8/workspaces/' . $this->workspaceId . '/clients');
+    }
+    
+    /**
+     * Get workspace groups
+     *
+     * @return stdClass
+     */
+    public function workspaceGroups()
+    {
+        return $this->sendGetMessage($this->baseUrl .'/v8/workspaces/' . $this->workspaceId . '/groups');
     }
 
     /**
@@ -24,7 +43,28 @@ trait WorkspaceTrait
     public function workspaceProjects()
     {
         return $this->sendGetMessage($this->baseUrl .'/v8/workspaces/' . $this->workspaceId . '/projects');
+    }
 
+    /**
+     * Get workspace tasks
+     *
+     * @return stdClass
+     */
+    public function workspaceTasks($active = true)
+    {
+        return $this->sendGetMessage($this->baseUrl .'/v8/workspaces/' . $this->workspaceId . '/tasks', [
+            'active' => $active
+        ]);
+    }
+    
+    /**
+     * Get workspace tags
+     *
+     * @return stdClass
+     */
+    public function workspaceTags()
+    {
+        return $this->sendGetMessage($this->baseUrl .'/v8/workspaces/' . $this->workspaceId . '/tags');
     }
 
 }
